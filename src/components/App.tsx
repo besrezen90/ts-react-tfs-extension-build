594db23034bf5a8fe75acde68317dbcc6936cbc2
Aleksandr <besrezen90@mail.ru>
2019-07-11 17:52:56 +0300

    init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import style from "./App.less";
 
-export default class App extends Component<{}, {}> {
-    render() {
-        return <div className={style.test}>Тестовая сборка для проверки</div>;
-    }
+interface IProps {}
+interface IState {}
+
+export default class App extends Component<IProps, IState> {
+	render() {
+		return <div className={style.test}>Тестовая сборка для проверки</div>;
+	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,50 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, "dist"),
-        port: 4200
-    },
-    optimization: {
-        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
-    },
-    plugins: [
-        new HTMLPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        })
-    ],
-    resolve: {
-        extensions: [".js", ".ts", ".jsx", ".tsx"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true
-                        }
-                    },
-                    "less-loader"
-                ]
-            }
-        ]
-    }
+	entry: "./src/index",
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, "dist"),
+		port: 4200
+	},
+	optimization: {
+		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+	},
+	plugins: [
+		new HTMLPlugin({
+			filename: "index.html",
+			template: "./src/index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "style.css"
+		})
+	],
+	resolve: {
+		extensions: [".js", ".ts", ".jsx", ".tsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|ts)x?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.less$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							modules: true
+						}
+					},
+					"less-loader"
+				]
+			}
+		]
+	}
 };
